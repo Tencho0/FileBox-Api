392bbf8b4110d92972869e98afd6c192b8682621
Tencho <tencho1011@gmail.com>
2024-07-30 10:27:22 +0300

    Add base file record model

---
+++ FileBoxApi-BackEnd/FileBoxApi-BackEnd/Models/FileRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace FileBoxApi_BackEnd.Models
+{
+    public class FileRecord
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Extension { get; set; } = string.Empty;
+
+        public byte[] Content { get; set; } = Array.Empty<byte>();
+
+        public DateTime UploadedAt { get; set; }
+    }
+}
